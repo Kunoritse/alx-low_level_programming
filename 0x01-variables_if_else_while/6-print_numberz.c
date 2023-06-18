59d1f13d826fcb8ecc635115791b7b65ffc16bbf
kunoritse <aaronkunoritse@gmail.com>
2023-06-18 15:15:56 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints from 0 to 9.
+ *
+ * Retun: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
